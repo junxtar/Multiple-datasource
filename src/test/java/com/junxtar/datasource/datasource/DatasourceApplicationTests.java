792ed88b2ebe26de2fb3c1e363f64c0272e7e38b
junxtar <aa1870088@naver.com>
2025-05-07 07:51:14 +0900

    git init

---
+++ src/test/java/com/junxtar/datasource/datasource/DatasourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junxtar.datasource.datasource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatasourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
